cc8a91b5b5d78f18016c1c9534a83bd26236a599
suresh-roshi <sureshroshi.d@gmail.com>
2023-12-17 23:54:33 +0530

    test jenkins

--- src/pages/blogs/HomePage.js
+++ src/pages/blogs/HomePage.js
@@ -5,7 +5,7 @@ function HomePage() {
     <>
       <div className="text-center fw-bold p-5 m-5 display-5">
         Explore the World of <br />{" "}
-        <span style={{ color: "#5869DA" }}>Torry Insight Hub</span>
+        <span style={{ color: "#5869DA" }}>Torry Insight Hub </span>
       </div>
       {/* <main className="bg-grey pb-30">
         <div className="container pt-30">
