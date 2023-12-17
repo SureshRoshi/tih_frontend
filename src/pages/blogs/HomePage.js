fb1271b05934079b89985fedf32ab407df3c54a4
suresh-roshi <sureshroshi.d@gmail.com>
2023-12-17 23:38:45 +0530

    added hero-section

--- src/pages/blogs/HomePage.js
+++ src/pages/blogs/HomePage.js
@@ -3,10 +3,175 @@ import React from "react";
 function HomePage() {
   return (
     <>
-      <div className="text-center fw-bold p-5 m-5 display-5">
+      {/* <div className="text-center fw-bold p-5 m-5 display-5">
         Explore the World of <br />{" "}
         <span style={{ color: "#5869DA" }}>Torry Insight Hub</span>
-      </div>
+      </div> */}
+      <main className="bg-grey pb-30">
+        <div className="container pt-30">
+          <div className="featured-slider-3 position-relative">
+            <div className="slider-3-arrow-cover"></div>
+            <div className="featured-slider-3-items">
+              <div className="slider-single overflow-hidden border-radius-10">
+                <div className="post-thumb position-relative">
+                  <div
+                    className="thumb-overlay position-relative"
+                    style={{
+                      backgroundImage: "url(assets/imgs/news/news-16.jpg)",
+                    }}
+                  >
+                    <div className="post-content-overlay">
+                      <div className="container">
+                        <div className="entry-meta meta-0 font-small mb-20">
+                          <a href="category.html" tabindex="0">
+                            <span className="post-cat text-info text-uppercase">
+                              Travel
+                            </span>
+                          </a>
+                          <a href="category.html" tabindex="0">
+                            <span className="post-cat text-warning text-uppercase">
+                              Animal
+                            </span>
+                          </a>
+                        </div>
+                        <h1 className="post-title mb-20 font-weight-900 text-white">
+                          <a
+                            className="text-white"
+                            href="single.html"
+                            tabindex="0"
+                          >
+                            How to Visit Bali's Monkey Forest
+                          </a>
+                        </h1>
+                        <div className="entry-meta meta-1 font-small text-white mt-10 pr-5 pl-5">
+                          <span className="post-on">26 August 2023</span>
+                          <span className="hit-count has-dot">18k Views</span>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className="slider-single overflow-hidden border-radius-10">
+                <div className="post-thumb position-relative">
+                  <div
+                    className="thumb-overlay position-relative"
+                    style={{
+                      backgroundImage: "url(assets/imgs/news/news-17.jpg)",
+                    }}
+                  >
+                    <div className="post-content-overlay">
+                      <div className="container">
+                        <div className="entry-meta meta-0 font-small mb-20">
+                          <a href="category.html" tabindex="0">
+                            <span className="post-cat text-info text-uppercase">
+                              Lifestyle
+                            </span>
+                          </a>
+                          <a href="category.html" tabindex="0">
+                            <span className="post-cat text-warning text-uppercase">
+                              Destinations
+                            </span>
+                          </a>
+                        </div>
+                        <h1 className="post-title mb-20 font-weight-900 text-white">
+                          <a
+                            className="text-white"
+                            href="single.html"
+                            tabindex="0"
+                          >
+                            Abstract Australia from Above
+                          </a>
+                        </h1>
+                        <div className="entry-meta meta-1 font-small text-white mt-10 pr-5 pl-5">
+                          <span className="post-on">15 September 2023</span>
+                          <span className="hit-count has-dot">23k Views</span>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className="slider-single overflow-hidden border-radius-10">
+                <div className="post-thumb position-relative">
+                  <div
+                    className="thumb-overlay position-relative"
+                    style={{
+                      backgroundImage: "url(assets/imgs/news/news-18.jpg)",
+                    }}
+                  >
+                    <div className="post-content-overlay">
+                      <div className="container">
+                        <div className="entry-meta meta-0 font-small mb-20">
+                          <a href="category.html" tabindex="0">
+                            <span className="post-cat text-warning text-uppercase">
+                              Travel Tips
+                            </span>
+                          </a>
+                        </div>
+                        <h1 className="post-title mb-20 font-weight-900 text-white">
+                          <a
+                            className="text-white"
+                            href="single.html"
+                            tabindex="0"
+                          >
+                            Tips for Scuba Diving the Great Barrier Reef
+                          </a>
+                        </h1>
+                        <div className="entry-meta meta-1 font-small text-white mt-10 pr-5 pl-5">
+                          <span className="post-on">15 September 2023</span>
+                          <span className="hit-count has-dot">17k Views</span>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className="slider-single overflow-hidden border-radius-10">
+                <div className="post-thumb position-relative">
+                  <div
+                    className="thumb-overlay position-relative"
+                    style={{
+                      backgroundImage: "url(assets/imgs/news/news-19.jpg)",
+                    }}
+                  >
+                    <div className="post-content-overlay">
+                      <div className="container">
+                        <div className="entry-meta meta-0 font-small mb-20">
+                          <a href="category.html" tabindex="0">
+                            <span className="post-cat text-info text-uppercase">
+                              Hotel
+                            </span>
+                          </a>
+                          <a href="category.html" tabindex="0">
+                            <span className="post-cat text-warning text-uppercase">
+                              Healthy
+                            </span>
+                          </a>
+                        </div>
+                        <h1 className="post-title mb-20 font-weight-900 text-white">
+                          <a
+                            className="text-white"
+                            href="single.html"
+                            tabindex="0"
+                          >
+                            Staying at the Hilton Seychelles Northolme Resort &
+                            Spa
+                          </a>
+                        </h1>
+                        <div className="entry-meta meta-1 font-small text-white mt-10 pr-5 pl-5">
+                          <span className="post-on">22 September 2023</span>
+                          <span className="hit-count has-dot">16k Views</span>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </main>
     </>
   );
 }
