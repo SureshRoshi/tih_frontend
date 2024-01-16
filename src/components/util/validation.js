68888526ff85fafcc26c96a66abd578ddd64914b
SureshRoshi <sureshroshi007@gmail.com>
2024-01-16 16:33:53 +0530

    updated validation

--- src/components/util/validation.js
+++ src/components/util/validation.js
@@ -5,3 +5,11 @@ export function isEmpId(value) {
 export function isPassword(value, minLength = 6) {
   return value.trim() !== "" && value.length >= minLength;
 }
+
+export function isEmail(value) {
+  return value.includes("@thbs");
+}
+
+export function isEqualsToOtherValue(value, otherValue) {
+  return value === otherValue;
+}
