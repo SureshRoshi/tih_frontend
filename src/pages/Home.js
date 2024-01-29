25195ff8114c9b6c651c74091d3459005fa01bd6
SureshRoshi <sureshroshi007@gmail.com>
2024-01-29 11:24:44 +0530

    updated Home

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,8 @@
 export default function Home() {
   return (
     <>
-      <div className="text-center fw-bold p-5 m-5 display-5">
-        <div className="text-center fw-bold p-5 m-5 display-5">
+      <div className="text-center fw-bold pt-5 mt-5 mb-3 pb-3 display-5">
+        <div className="text-center fw-bold pt-5 mt-5 mb-3 pb-3 display-5">
           Explore the World of <br />{" "}
           <span style={{ color: "#5869DA" }}>Torry Insight Hub</span>
         </div>
