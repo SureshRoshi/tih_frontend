9ff5d7529d6e43b2674b6186c2f68eaa681393ce
SureshRoshi <sureshroshi007@gmail.com>
2024-02-02 00:55:32 +0530

    updated navbar

--- src/components/Layout/Navbar.js
+++ src/components/Layout/Navbar.js
@@ -43,11 +43,48 @@ function Navbar() {
                       to={"/your-profile"}
                       className="btn btn-radius bg-primary text-white ml-15 font-small box-shadow"
                     >
-                      Profile
+                      <span className="d-flex align-items-center">
+                        <span>
+                          <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            width="16"
+                            height="16"
+                            fill="currentColor"
+                            className="bi bi-person-circle"
+                            viewBox="0 0 16 16"
+                          >
+                            <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0" />
+                            <path
+                              fillRule="evenodd"
+                              d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8m8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1"
+                            />
+                          </svg>
+                        </span>
+                        <span className="ms-1">Profile</span>
+                      </span>
                     </Link>
                     <Form method="POST" action="/logout">
                       <button className="btn btn-radius bg-primary text-white ml-15 font-small box-shadow">
-                        Logout
+                        <span className="d-flex align-items-center">
+                          <span className="me-1">Logout</span>
+                          <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            width="16"
+                            height="16"
+                            fill="currentColor"
+                            className="bi bi-box-arrow-right"
+                            viewBox="0 0 16 16"
+                          >
+                            <path
+                              fillRule="evenodd"
+                              d="M10 12.5a.5.5 0 0 1-.5.5h-8a.5.5 0 0 1-.5-.5v-9a.5.5 0 0 1 .5-.5h8a.5.5 0 0 1 .5.5v2a.5.5 0 0 0 1 0v-2A1.5 1.5 0 0 0 9.5 2h-8A1.5 1.5 0 0 0 0 3.5v9A1.5 1.5 0 0 0 1.5 14h8a1.5 1.5 0 0 0 1.5-1.5v-2a.5.5 0 0 0-1 0z"
+                            />
+                            <path
+                              fillRule="evenodd"
+                              d="M15.854 8.354a.5.5 0 0 0 0-.708l-3-3a.5.5 0 0 0-.708.708L14.293 7.5H5.5a.5.5 0 0 0 0 1h8.793l-2.147 2.146a.5.5 0 0 0 .708.708z"
+                            />
+                          </svg>
+                        </span>
                       </button>
                     </Form>
                   </>
