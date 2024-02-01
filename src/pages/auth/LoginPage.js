fd35ce46a3a0670d78eeb4fdff30e604c4b96108
SureshRoshi <sureshroshi007@gmail.com>
2024-02-01 22:40:22 +0530

    updated login & signup

--- src/pages/auth/LoginPage.js
+++ src/pages/auth/LoginPage.js
@@ -1,8 +1,12 @@
-import React from "react";
+import React, { useEffect } from "react";
 
 import LoginForm from "../../components/UI/LoginForm";
 
 function LoginPage() {
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
+
   return <LoginForm />;
 }
 
--- src/pages/auth/SignupPage.js
+++ src/pages/auth/SignupPage.js
@@ -1,6 +1,11 @@
+import { useEffect } from "react";
 import SignupForm from "../../components/UI/SignupForm";
 
 function SignupPage() {
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
+
   return <SignupForm />;
 }
 export default SignupPage;
