15230eb354c08610d4dd0d6c519030dc3a9c7e69
SureshRoshi <sureshroshi007@gmail.com>
2024-02-04 15:44:08 +0530

    updated author and profile

--- src/pages/profile/Profile.js
+++ src/pages/profile/Profile.js
@@ -61,36 +61,26 @@ export default function ProfilePage() {
                   </h3>
                   <p className="text-muted" />
                   <div className="author-description text-muted"></div>
-                  {/* <strong className="text-muted">Follow: </strong>
-                <ul className="header-social-network d-inline-block list-inline color-white mb-20">
-                  <li className="list-inline-item">
-                    <a className="fb" href="#" target="_blank" title="Facebook">
-                      <i className="elegant-icon social_facebook"></i>
-                    </a>
-                  </li>
-                  <li className="list-inline-item">
-                    <Link
-                      className="tw"
-                      href="#"
-                      target="_blank"
-                      title="Tweet now"
-                    >
-                      <i className="elegant-icon social_twitter"></i>
-                    </Link>
-                  </li>
-                  <li className="list-inline-item">
-                    <Link
-                      className="pt"
-                      href="#"
-                      target="_blank"
-                      title="Pin it"
-                    >
-                      <i className="elegant-icon social_pinterest"></i>
-                    </Link>
-                  </li>
-                </ul> */}
+                  <strong className="text-muted">Follow: </strong>
+                  <ul className="header-social-network d-inline-block list-inline color-white mb-20 ml-5">
+                    <li className="list-inline-item">
+                      <a className="gh" href="" target="_blank" title="GitHub">
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          width="16"
+                          height="16"
+                          fill="currentColor"
+                          className="bi bi-github"
+                          viewBox="0 0 16 16"
+                        >
+                          <path d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27s1.36.09 2 .27c1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.01 8.01 0 0 0 16 8c0-4.42-3.58-8-8-8" />
+                        </svg>
+                      </a>
+                    </li>
+                  </ul>
+                  <br />
                   <Link
-                    className="btn btn-radius bg-primary text-white ml-15 font-small box-shadow"
+                    className="btn btn-radius bg-primary text-white font-small box-shadow"
                     to={"/blogs/add-post"}
                   >
                     <span className="d-flex align-items-center">
